e8c9d8bfe48e0fe2ba649727ee63612d3e0bb40d
rick0829 <earl0829@gmail.com>
2022-04-26 21:19:00 -0700

    Added more pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let pokemonList = [
-  {name: 'Charizard'},
-  {height: 1.7},
-  {eggGroups: 'Moster', 'Dragon'}
+  { name: 'Charizard', height: 5.58, types: ['Fire', 'Flying'] },
+  { name: 'Bulbasaur', height: 2.33, types: ['Grass', 'Poison'] },
+  { name: 'Wartortle', height: 3.25, types: ['Water'] },
+  { name: 'Metapod', height: 2.33, types: ['Bug'] },
+  { name: 'Butterfree', height: 3.58, types: ['Bug', 'Flying'] },
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' Height: ' + pokemonList[i].height);
+}
