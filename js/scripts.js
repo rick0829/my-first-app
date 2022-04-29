e3a21a86aaa6a3897ba209f9b113d3617dd05fa4
rick0829 <earl0829@gmail.com>
2022-04-29 13:43:12 -0700

    First basic styling for the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 let pokemonList = [
-  { name: 'Charizard', height: 5.58, types: ['Fire', 'Flying'] },
-  { name: 'Bulbasaur', height: 2.33, types: ['Grass', 'Poison'] },
-  { name: 'Wartortle', height: 3.25, types: ['Water'] },
-  { name: 'Metapod', height: 2.33, types: ['Bug'] },
-  { name: 'Butterfree', height: 3.58, types: ['Bug', 'Flying'] },
+  {
+    name: 'Charizard', height: 5.58, types: ['Fire', 'Flying'],
+  },
+  {
+    name: 'Bulbasaur', height: 2.33, types: ['Grass', 'Poison'],
+  },
+  {
+    name: 'Wartortle', height: 3.25, types: ['Water'],
+  },
+  {
+    name: 'Metapod', height: 2.33, types: ['Bug'],
+  },
+  {
+    name: 'Butterfree', height: 3.58, types: ['Bug', 'Flying'],
+  },
 ];
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' Height: ' + pokemonList[i].height);
+  // Anything over a height of the 3 is considered big
+  if (pokemonList[i].height > 3) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow that\'s big!');
+  } else {
+    // Applies to Pokemon who are under 3
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+  }
 }
